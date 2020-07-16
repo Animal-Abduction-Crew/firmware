76afeceb29839eac557983f9e844d31877b922ce
Animal Abduction Crew <github@esslinger.dev>
2020-07-16 17:40:45 +0200

    add stuff

--- src/serious/test_driver.py
+++ src/serious/test_driver.py
@@ -8,8 +8,21 @@ left_motor = Motor(pi, [12,7,8])
 right_motor = Motor(pi, [18,15,14])
 driver = Driver(pi, left_motor, right_motor)
 
-for i in range(1,20):
-    driver.turn_left(0.02, 100)
-    time.sleep(0.5)
+done = False
+
+while not done:
+
+    action = input('what do?')
+
+    if action == 'w':
+        driver.forward(1,100)
+    elif action == 's':
+        driver.reverse(1,100)
+    elif action == 'a':
+        driver.turn_left(0.2,100)
+    elif action == 'd':
+        driver.turn_right(0.2,100)
+    elif action == 'exit':
+        done = True
 
 pi.stop()
