ea64200f025ffcca96daefc70b2ac573dcac7f5d
skyfrk <simon@esslinger.dev>
2020-07-18 12:53:36 +0000

    feat: new structure

--- src/serious/strats/default_routine.py
+++
@@ -1,8 +0,0 @@
-class DefaultRoutine:
-
-    def __init__(self, ):
-        print('init default routine')
-
-    def callback_line_detected():
-        print('i am the line detection callback')
-    
