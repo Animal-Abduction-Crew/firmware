1a2c0b0575a0c8ca30c20597fe166fee8b4c6654
Animal Abduction Crew <github@esslinger.dev>
2020-07-21 09:07:51 +0200

    fix: distance check

--- src/serious/main.py
+++ src/serious/main.py
@@ -153,9 +153,8 @@ while not done:
 
                 if detection['name'] == animal and detection['confidence'] > min_confidence:
                     if adv_driver.adjust_to_target(detection):
-
                         # should be at least 1m infront
-                        if detection['width'] >= 40:
+                        if detection['width'] < 40:
                             search()
                         else:
                             push_it_out()
